1bc63b8672ab410068089412ee2d8238e094553b
Francesca-A01 <francescafiless@gmail.com>
2024-09-09 15:32:15 -0600

    Update menu main.js

--- main.js
+++ main.js
@@ -11,3 +11,9 @@ function muestra_oculta(id) {
     }
 }
 
+    const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
